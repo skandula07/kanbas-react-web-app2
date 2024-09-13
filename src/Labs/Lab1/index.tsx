a5c107ef71bd0236b8f7e468ab11637e195d4350
sree :) <kandula.s@northeastern.edu>
2024-09-13 16:16:58 -0400

    reformatted lab1

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -3,25 +3,26 @@ import Table from "./Table";
 import Forms from "./Forms";
 
 export default function Lab1() {
-    return (
-      <div id="wd-lab1">
+  return (
+    <div id="wd-lab1">
+      <h2>Lab 1</h2>
 
-<div id="wd-p-tag">
+      <div id="wd-p-tag">
         <h4>Paragraph Tag</h4>
         <p id="wd-p-1"> ... </p>
         <p id="wd-p-2">
-This is the first paragraph. The paragraph tag is used to format
-vertical gaps between long pieces of text like this one.
+          This is the first paragraph. The paragraph tag is used to format
+          vertical gaps between long pieces of text like this one.
         </p>
         <p id="wd-p-3">
-This is the second paragraph. Even though there is a deliberate white
-gap between the paragraph above and this paragraph, by default
-browsers render them as one contiguous piece of text as shown here on
-the right.
+          This is the second paragraph. Even though there is a deliberate white
+          gap between the paragraph above and this paragraph, by default
+          browsers render them as one contiguous piece of text as shown here on
+          the right.
         </p>
         <p id="wd-p-4">
-This is the third paragraph. Wrap each paragraph with the paragraph
-tag to tell browsers to render the gaps.
+          This is the third paragraph. Wrap each paragraph with the paragraph
+          tag to tell browsers to render the gaps.
         </p>
       </div>
 
@@ -29,10 +30,7 @@ tag to tell browsers to render the gaps.
       <Table />
       <Forms />
 
-        <h2>Lab 1</h2>
-        <h4>Excersize 1.1</h4>
-        <h4>Excersize 1.2</h4>
-      </div> 
-    );
-  } 
-  
+      <h6>~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~</h6>
+    </div>
+  );
+}
