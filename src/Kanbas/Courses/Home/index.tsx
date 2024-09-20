e9ba16ebb9b09d2f85f58fc519a3a1170413ad6a
sree :) <kandula.s@northeastern.edu>
2024-09-20 13:43:58 -0400

    updated 'modules' and 'status'

--- src/Kanbas/Courses/Home/index.tsx
+++ src/Kanbas/Courses/Home/index.tsx
@@ -3,9 +3,23 @@ import CourseStatus from "./Status";
 export default function Home() {
   return (
     <table id="wd-home">
+      <button>Collapse All</button>
+      <button>View Progress</button>
+
+      <select id="wd-select-one-genre">
+        <option selected value="COMEDY">Publish All</option>
+        <option value="SCIFI">Course 1234</option>
+      </select>
+      <button>+ Module</button>
+
       <tr>
-        <td valign="top"> <Modules /> </td>
-        <td valign="top"> <CourseStatus /> </td>
+        <td valign="top">
+          <Modules />
+        </td>
+        <td valign="top">
+          <CourseStatus />
+        </td>
       </tr>
     </table>
-);}
+  );
+}
