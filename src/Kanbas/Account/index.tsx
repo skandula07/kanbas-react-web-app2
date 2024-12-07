4d41c581a79a7d1afa274684b066bba544024e24
sree :) <kandula.s@northeastern.edu>
2024-12-07 15:00:22 -0500

    a6 initial commit

--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -3,22 +3,27 @@ import Profile from "./Profile";
 import Signin from "./Signin";
 import Signup from "./Signup";
 import AccountNavigation from "./Navigation";
+import Users from "./Users";
+import { useSelector } from "react-redux";
 export default function Account() {
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+
   return (
     <div>
-
 
       <div className="d-flex">
       <AccountNavigation />
       <div className="m-3">
       <Routes>
-              <Route
-                path="/"
-                element={<Navigate to="/Kanbas/Account/Signin" />}
-              />
+              <Route path="/" element={ <Navigate to={currentUser ? "/Kanbas/Account/Profile" : "/Kanbas/Account/Signin" } /> } />
+
               <Route path="/Signin" element={<Signin />}  />
               <Route path="/Profile" element={<Profile />} />
               <Route path="/Signup" element={<Signup />} />
+              <Route path="/Users" element={<Users />} />
+              <Route path={`/Users/:uid`} element={<Users />} />
+
+
             </Routes>
 
       </div>
